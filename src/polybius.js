b811c609581ff403f0b9ea8b3450851797efb1c4
meleciosilva <mpsilva94@gmail.com>
2021-01-22 15:51:09 -0500

    refactored function with only one forEach loop

--- src/polybius.js
+++ src/polybius.js
@@ -5,46 +5,44 @@ const polybiusModule = (function () {
   function polybius(input, encode = true) {
     let result = '';
     if (!encode && input.length % 2 !== 0) return false;
-    if (encode) {
-      const inputCharacters = input.toLowerCase().split('');
-      inputCharacters.forEach((inputChar) => {
-        const match = alphabet.find((letter) => letter === inputChar);
-        const numMatch = numbers.find((number) => alphabet.indexOf(match) === numbers.indexOf(number));
-        // inserts spaces and non-alphabetic symbols if included in input
-        if (!alphabet.includes(inputChar)) {
-          result += inputChar;
-        } else if (inputChar === 'i' || inputChar === 'j') {
-          result += '42';
+    let inputCharacters; // will split input into multiple items in an array
+    // when decoding, converts input into an array of 2-digit values/strings
+    if (!encode) { 
+      inputCharacters = [];
+      for (let i = 0; i < input.length-1; i += 2) {  
+        if (input[i] === ' ') {
+          inputCharacters.push(input[i]);
+          i--; // adjusts for loop so next character in input is checked for a number
         } else {
-          result += numMatch;
-        }  
-      });
-    // conditional to execute if called to decode
-    } else { 
-        // when decoding, converts input into an array of 2-digit values/strings
-        const inputNumbers = [];
-        for (let i = 0; i < input.length-1; i+=2) {  
-          if (input[i] === ' ') {
-            inputNumbers.push(input[i]);
-            i--; // adjusts for loop so nect character in input is checked for a number
-          } else {
-          inputNumbers.push(input[i] + input[i + 1]);
-          }
+          inputCharacters.push(input[i] + input[i + 1]);
         }
-        // matches each inputNumber to its corresponding letter
-        inputNumbers.forEach((inputNum) => {
-          const match = numbers.find((number) => number === inputNum);
-          const charMatch = alphabet.find((letter) => numbers.indexOf(match) === alphabet.indexOf(letter));
-        // inserts a space if included in the input
-          if (!numbers.includes(inputNum)) {
-            result += inputNum;
-          } else if (inputNum === '42') {
-            result += '(i/j)';
-          } else {
-            result += charMatch;
-          }
-        });
+      } 
+    } else {
+      // splits input of text into single character items
+      inputCharacters = input.toLowerCase().split('');
+    }
+    // iterates through each input character, finds the matching character in alphabet/numbers, and finds the corresposing leter/number to convert to
+    inputCharacters.forEach((inputChar) => {
+      let match; // will identify if input matches 
+      let matchConvert; // will identify appropriate number/letter corresponding to input character
+      if (encode) {
+        match = alphabet.find((letter) => letter === inputChar);
+        matchConvert = numbers.find((number) => alphabet.indexOf(match) === numbers.indexOf(number));
+      } else {
+        match = numbers.find((number) => number === inputChar);
+        matchConvert = alphabet.find((letter) => numbers.indexOf(match) === alphabet.indexOf(letter));
       }
+      // inserts spaces and non-alphabetic symbols if included in input
+      if ( encode && !alphabet.includes(inputChar) || !encode && !numbers.includes(inputChar) ) {
+        result += inputChar;
+      } else if ( inputChar === 'i' || inputChar === 'j' ) {
+        result += '42';
+      } else if (inputChar === '42') {
+        result += '(i/j)';
+      } else {
+        result += matchConvert;
+      }  
+    });
     return result;
   }
   return {
