429db576150debbbd377f06568ad1e237f56bc90
meleciosilva <mpsilva94@gmail.com>
2021-01-22 18:26:29 -0500

    refactored polybius function to utilize helper function

--- src/polybius.js
+++ src/polybius.js
@@ -1,11 +1,19 @@
 const polybiusModule = (function () {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
   const numbers = ['11','21','31','41','51','12','22','32','42','42','52','13','23','33','43','53','14','24','34','44','54','15','25','35','45','55'];
-
+  // helper function: identifies which character in "endCharacters" corresponds to input character in "startCharacters" and returns it
+  function characterConversion(startCharacters, endCharacters, currentCharacter) {
+    if (!startCharacters.includes(currentCharacter)) return currentCharacter;
+    if ( currentCharacter === 'i' || currentCharacter === 'j' ) return '42';
+    if (currentCharacter === '42') return '(i/j)';
+    const match = startCharacters.find((char) => char === currentCharacter);
+    const matchConvert = endCharacters.find((char) => startCharacters.indexOf(match) === endCharacters.indexOf(char));
+    return matchConvert;
+  }
   function polybius(input, encode = true) {
     let result = '';
     if (!encode && input.length % 2 !== 0) return false;
-    let inputCharacters; // will split input into multiple items in an array
+    let inputCharacters;
     // when decoding, converts input into an array of 2-digit values/strings
     if (!encode) { 
       inputCharacters = [];
@@ -18,30 +26,16 @@ const polybiusModule = (function () {
         }
       } 
     } else {
-      // splits input of text into single character items
+      // when encoding, splits input of text into single character items
       inputCharacters = input.toLowerCase().split('');
     }
-    // iterates through each input character, finds the matching character in alphabet/numbers, and finds the corresposing leter/number to convert to
+    // iterates through each input character and finds the character match to convert to
     inputCharacters.forEach((inputChar) => {
-      let match; // will identify if input matches 
-      let matchConvert; // will identify appropriate number/letter corresponding to input character
       if (encode) {
-        match = alphabet.find((letter) => letter === inputChar);
-        matchConvert = numbers.find((number) => alphabet.indexOf(match) === numbers.indexOf(number));
+        result += characterConversion(alphabet, numbers, inputChar);
       } else {
-        match = numbers.find((number) => number === inputChar);
-        matchConvert = alphabet.find((letter) => numbers.indexOf(match) === alphabet.indexOf(letter));
-      }
-      // inserts spaces and non-alphabetic symbols if included in input
-      if ( encode && !alphabet.includes(inputChar) || !encode && !numbers.includes(inputChar) ) {
-        result += inputChar;
-      } else if ( inputChar === 'i' || inputChar === 'j' ) {
-        result += '42';
-      } else if (inputChar === '42') {
-        result += '(i/j)';
-      } else {
-        result += matchConvert;
-      }  
+        result += characterConversion(numbers, alphabet, inputChar);
+      } 
     });
     return result;
   }
